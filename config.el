71eb7dc7d4ea4b1c461b622dcc25de1393af0f4e
Yasin Shuman <hello@yasinshuman.com>
2024-09-13 16:12:32 -0700

    Auto-commit: 2024-09-13 16:12:32

--- config.el
+++ config.el
@@ -240,5 +240,5 @@ Provides feedback if the commit and push were successful."
         '(("m" "Meeting Recording" plain
            "* %<%Y-%m-%d> %^{Title}\n:PROPERTIES:\n:Attendees: %^{Attendees}\n:Tags: %^{Tags}\n:END:\n\n** Summary\n%?\n\n** Video Link\n%^{Video Link}"
            :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                              "#+title: mtg recordings\n#+filetags: %^{Tags}\n")
+                              "#+title: %^{Title}\n#+filetags: %^{Tags}\n")
            :unnarrowed t))))
